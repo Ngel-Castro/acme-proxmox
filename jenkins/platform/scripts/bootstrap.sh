17c536bc52fb476f6952c360d40e9377129bbb69
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-10 15:58:34 -0400

    getting ready for merge after testing using specific inventory for environment and test prod deployment too

--- jenkins/platform/scripts/bootstrap.sh
+++ jenkins/platform/scripts/bootstrap.sh
@@ -3,7 +3,7 @@ export ENV=$1
 
 export SECRETS_FILE=~/.personal_vault/todopoderoso
 export JENKINS_PLATFORM_INFRA=jenkins/platform/infra/
-export JENKINS_PLATFORM_ANSIBLE=jenkins/platform/ansible/
+export JENKINS_PLATFORM_ANSIBLE=platform/ansible/
 ## TODO change path to be more absolute. right now the assumption is that the .tfvars are inside `jenkins/platform` folder.
 export JENKINS_VARS=env/${ENV}/tofu.tfvars
 export SERVER_SSH_KEY=~/.ssh/cluster_alma
@@ -33,7 +33,7 @@ echo "Deploying infrastructure"
 tofu plan -var-file=${JENKINS_VARS} -var="proxmox_token_id=${PROXMOX_TOKEN_ID}" -var="proxmox_token_secret=${PROXMOX_TOKEN_SECRET}"
 tofu apply --auto-approve -var-file=${JENKINS_VARS} -var="proxmox_token_id=${PROXMOX_TOKEN_ID}" -var="proxmox_token_secret=${PROXMOX_TOKEN_SECRET}"
 echo "Moving inventory YAML to inventory location in ansible"
-cp inventory_${ENV}.yaml $initialLocation/${JENKINS_PLATFORM_ANSIBLE}/inventory/ci/
+cp inventory_${ENV}.yaml $initialLocation/jenkins/${JENKINS_PLATFORM_ANSIBLE}/inventory/ci/
 
 cd $initialLocation
 cd jenkins
