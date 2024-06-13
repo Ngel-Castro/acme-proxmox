05ef0971a952efb543572cad1e538f1b38e932ff
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-13 17:27:49 -0400

    getting ready for E2E deployment

--- jenkins/platform/scripts/bootstrap.sh
+++ jenkins/platform/scripts/bootstrap.sh
@@ -7,6 +7,7 @@ export JENKINS_PLATFORM_ANSIBLE=platform/ansible/
 ## TODO change path to be more absolute. right now the assumption is that the .tfvars are inside `jenkins/platform` folder.
 export JENKINS_VARS=env/${ENV}/tofu.tfvars
 export SERVER_SSH_KEY=~/.ssh/cluster_alma
+export ADMIN_SSH_KEY=~/.ssh/admin_key
 
 echo "Bootstrapping Jenkins"
 
@@ -41,5 +42,5 @@ cd jenkins
 echo "Running ansible"
 export INVENTORY_FILE=${JENKINS_PLATFORM_ANSIBLE}/inventory/ci/inventory_${ENV}.yaml
 
-ansible-playbook site.yml -i ${INVENTORY_FILE} --extra-vars "cluster_ssh_key=${SERVER_SSH_KEY} ansible_sudo_pass=$CLUSTER_PASSWORD git_user=${GITHUB_USERNAME} git_token=${GITHUB_TOKEN} proxmox_token_id=${PROXMOX_TOKEN_ID} proxmox_token_secret=${PROXMOX_TOKEN_SECRET}"
+ansible-playbook site.yml -i ${INVENTORY_FILE} --extra-vars "cluster_ssh_key=${SERVER_SSH_KEY} ansible_sudo_pass=$CLUSTER_PASSWORD git_user=${GITHUB_USERNAME} git_token=${GITHUB_TOKEN} proxmox_token_id=${PROXMOX_TOKEN_ID} proxmox_token_secret=${PROXMOX_TOKEN_SECRET} ssh_admin_key_path=${ADMIN_SSH_KEY}"
 
