27b3c817c4f514c03f854e13f0c633a077a6b499
Angel Castro <castrobasurto.angel@gmail.com>
2024-06-21 11:56:32 -0400

    re-adjustments of status

--- platform/scripts/bootstrap.sh
+++ platform/scripts/bootstrap.sh
@@ -3,7 +3,7 @@ export ENV=$1
 
 export SECRETS_FILE=.env
 export PLATFORM_INFRA=platform/infra/
-export PLATFORM_ANSIBLE=platform/ansible/
+export PLATFORM_ANSIBLE=ansible/
 export PLATFORM_VARS=env/${ENV}/tofu.tfvars
 export SERVER_SSH_KEY=~/.ssh/cluster_alma
 export ADMIN_SSH_KEY=~/.ssh/cluster_alma
@@ -33,13 +33,13 @@ echo "Deploying infrastructure"
 tofu plan -var-file=${PLATFORM_VARS} -var="proxmox_token_id=${PROXMOX_TOKEN_ID}" -var="proxmox_token_secret=${PROXMOX_TOKEN_SECRET}"
 tofu apply --auto-approve -var-file=${PLATFORM_VARS} -var="proxmox_token_id=${PROXMOX_TOKEN_ID}" -var="proxmox_token_secret=${PROXMOX_TOKEN_SECRET}"
 echo "Moving inventory YAML to inventory location in ansible"
-cp inventory_${ENV}.yaml $initialLocation/${PLATFORM_ANSIBLE}/inventory/ci/
+cp inventory_${ENV}.yaml $initialLocation/platform/${PLATFORM_ANSIBLE}/inventory/ci/
 
 cd $initialLocation
 cd platform
 
 echo "Running ansible"
-export INVENTORY_FILE=${PLATFORM_ANSIBLE}/inventory/ci/inventory_${ENV}.yaml
+export INVENTORY_FILE="${PLATFORM_ANSIBLE}/inventory/ci/inventory_${ENV}.yaml"
 
 ansible-playbook site.yml -i ${INVENTORY_FILE} --extra-vars "platform_environment=${ENV} cluster_ssh_key=${SERVER_SSH_KEY} ansible_sudo_pass=$CLUSTER_PASSWORD git_user=${GITHUB_USERNAME} git_token=${GITHUB_TOKEN} proxmox_token_id=${PROXMOX_TOKEN_ID} proxmox_token_secret=${PROXMOX_TOKEN_SECRET} ssh_admin_key_path=${ADMIN_SSH_KEY}"
 
